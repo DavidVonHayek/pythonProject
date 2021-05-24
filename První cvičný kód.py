28c5fb2284ac1c5b05b339c3f697128cc77ed782
David Spies <spies001@gmail.com>
2021-05-25 00:05:49 +0200

    Initial commit

---
+++ První cvičný kód.py
@@ -0,0 +1,17 @@
+# Vytvor list 'tyden' a uloz dny v tydnu v podobe stringu
+tyden = ["pondeli","utery","streda","ctvrtek","patek","sobota","nedele"]
+
+# Ziskej vyber dne od uzivatele
+cislo_dne = int(input("Vyber den v tydnu dle jeho cisla, kde 1 je pondeli"))
+if cislo_dne < 1 or cislo_dne > 7:
+    print("Vybrali jste cislo mimo rozsah")
+else:
+    tip_prvniho_pismena = input("jake je prvni pismeno?")
+# Ziskej den z listu na zaklade hodnoty ziskane od uzivatele
+vybrany_den = tyden[cislo_dne - 1]
+prvni_pismeno_dne = vybrany_den[0]
+
+if prvni_pismeno_dne == tip_prvniho_pismena:
+  print("Super, správný tip prvního písmene")
+else:
+  print("Špatný tip prvního písmene")
